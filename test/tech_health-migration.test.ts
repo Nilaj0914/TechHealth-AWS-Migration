9db6677bcbef353b05702eb9b44a87a9cdd71891
Nilaj0914 <nilaj.majumder0814@gmail.com>
2025-08-13 10:31:20 +0530

    Initial commit

---
+++ test/tech_health-migration.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TechHealthMigration from '../lib/tech_health-migration-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/tech_health-migration-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TechHealthMigration.TechHealthMigrationStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
